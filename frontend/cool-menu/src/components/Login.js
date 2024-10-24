f7dd252d9392bf3cff2d089e52db0eae47bc18ff
hjm <sjtu@qq.com>
2024-10-24 19:09:44 +0800

    merge cw

--- frontend/cool-menu/src/components/Login.js
+++ frontend/cool-menu/src/components/Login.js
@@ -1,62 +1,99 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './css/login.css';
 
 // 定义样式
 const styles = {
-    loginContainer: {
-      maxWidth: '400px',
-      maxHeight: '250px',
-      margin: '150px auto',
-      padding: '30px 50px 100px',
-      borderRadius: '8px',
-      boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-      backgroundColor: '#f9f9f9',
-      transition: 'all 0.3s ease',
-    },
-    loginTitle: {
-      textAlign: 'center',
-      marginBottom: '20px',
-      color: '#333',
-    },
-    formGroup: {
-      marginBottom: '15px',
-    },
-    formInput: {
-      width: '100%',
-      padding: '10px',
-      borderRadius: '4px',
-      border: '1px solid #ccc',
-      boxShadow: 'inset 0 1px 3px rgba(0, 0, 0, 0.1)',
-      transition: 'border-color 0.2s',
-    },
-    formInputFocus: {
-      borderColor: '#007bff',
-      outline: 'none',
-    },
-    loginButton: {
-      width: '100%',
-      textAlign: 'center',
-      padding: '10px',
-      border: 'none',
-      borderRadius: '4px',
-      backgroundColor: '#007bff',
-      color: 'white',
-      fontSize: '16px',
-      cursor: 'pointer',
-      transition: 'background-color 0.2s',
-    },
-    loginButtonHover: {
-      backgroundColor: '#0056b3',
-    },
-    registerLink: {
-      display: 'block',
-      textAlign: 'right',
-      marginTop: '15px',
-      color: '#007bff',
-      cursor: 'pointer',
-    },
-  };
+  backgroundImage: {
+    backgroundImage: `url('/login.png')`,
+    backgroundSize: 'cover', // 使用 'cover' 使背景图覆盖整个容器
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'fixed', // 固定背景图
+    top: 0,
+    left: 0,
+    width: '100%', // 让宽度为 100%
+    height: '100%', // 让高度为 100%
+    zIndex: -1, // 确保背景在最底层
+  }, 
+  loginContainer: {
+    maxWidth: '300px',
+    maxHeight: '500px',
+    margin: '100px auto',
+    padding: '30px 50px 100px',
+    borderRadius: '8px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+    background: 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.2))', // 渐变背景，边缘更黑
+    //opacity: 0.7, // 背景透明度
+    transition: 'all 0.3s ease',
+  },
+  WebTitle: {
+    textAlign: 'center',
+    marginBottom: '30px',
+    fontSize: '44px',
+    fontWeight: 'bold',
+    background: 'linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)', // 设置您想要的渐变背景
+    WebkitBackgroundClip: 'text', // 剪裁背景到文本
+    color: 'transparent', // 文字颜色设置为透明
+  },
+  loginTitle: {
+    textAlign: 'center',
+    marginBottom: '30px',
+    color: 'white',
+    fontSize: '28px',
+    fontWeight: 'bold',
+  },
+  formGroup: {
+    marginBottom: '20px',
+  },
+  formInput: {
+    width: '70%',
+    padding: '10px',
+    border: 'none',
+    borderBottom: '3px solid white',
+    boxShadow: 'none',
+    transition: 'border-color 0.02s',
+    backgroundColor: 'transparent',
+    zIndex: 1,
+    outline: 'none',
+    color: 'white',
+    opacity: 0.8,
+    fontSize: '14px',
+  },
+  formInputFocus: {
+    borderColor: '1px solid white',
+    outline: 'none',
+  },
+  loginButton: {
+    marginBottom: '20px',
+    width: '50%',
+    textAlign: 'center',
+    padding: '10px',
+    border: 'none',
+    borderRadius: '20px',
+    background: 'linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)',
+    color: 'white',
+    fontSize: '16px',
+    cursor: 'pointer',
+    transition: 'background-color 0.2s',
+  },
+  loginButtonHover: {
+    backgroundColor: '#0056b3',
+  },
+  registerLink: {
+    display: 'block',
+    bottom: '10px',
+    left: '95%',
+    textAlign: 'right',
+    marginTop: '15px',
+    color: 'white',
+    opacity: 0.6,
+    cursor: 'pointer',
+    fontSize: '12px',
+    textDecoration: 'underline',
+  },
+};
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -79,45 +116,50 @@ function Login() {
   };
 
   return (
-    <div style={styles.loginContainer}>
-      <h2 style={styles.loginTitle}>欢迎登录</h2>
-      <form onSubmit={handleLogin}>
-        <div style={styles.formGroup}>
-          <label>用户名:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            style={styles.formInput}
-            onFocus={(e) => e.target.style.borderColor = styles.formInputFocus.borderColor}
-            onBlur={(e) => e.target.style.borderColor = ''}
-          />
+    <div style={styles.backgroundImage}>
+      <div style={styles.loginContainer}>
+        <h2 style={styles.WebTitle}>BOUNTY INN</h2>
+        <h2 style={styles.loginTitle}>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div style={styles.formGroup}>
+            <i className="fas fa-user" style={{ color: 'white', marginRight: '10px' }}></i> {/* 用户图标 */}
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              style={styles.formInput}
+              placeholder="UserName"
+              onFocus={(e) => e.target.style.borderColor = styles.formInputFocus.borderColor}
+              onBlur={(e) => e.target.style.borderColor = ''}
+            />
+          </div>
+          <div style={styles.formGroup}>
+            <i className="fas fa-lock" style={{ color: 'white', marginRight: '10px' }}></i> {/* 锁图标 */}
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={styles.formInput}
+              placeholder='UserPassword'
+              onFocus={(e) => e.target.style.borderColor = styles.formInputFocus.borderColor}
+              onBlur={(e) => e.target.style.borderColor = ''}
+            />
+          </div>
+          <button
+            type="submit"
+            style={styles.loginButton}
+            onMouseEnter={(e) => e.target.style.backgroundColor = styles.loginButtonHover.backgroundColor}
+            onMouseLeave={(e) => e.target.style.backgroundColor = styles.loginButton.backgroundColor}
+          >
+            Sign in
+          </button>
+        </form>
+        <div style={styles.registerLink} onClick={() => navigate('/register')}>
+          No account? Sign up
         </div>
-        <div style={styles.formGroup}>
-          <label>密码:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={styles.formInput}
-            onFocus={(e) => e.target.style.borderColor = styles.formInputFocus.borderColor}
-            onBlur={(e) => e.target.style.borderColor = ''}
-          />
-        </div>
-        <button
-          type="submit"
-          style={styles.loginButton}
-          onMouseEnter={(e) => e.target.style.backgroundColor = styles.loginButtonHover.backgroundColor}
-          onMouseLeave={(e) => e.target.style.backgroundColor = styles.loginButton.backgroundColor}
-        >
-          登录
-        </button>
-      </form>
-      <div style={styles.registerLink} onClick={() => navigate('/register')}>
-        注册账号
       </div>
     </div>
-  );
+  );  
 }
 
 export default Login;
--- frontend/cool-menu/src/components/Register.js
+++ frontend/cool-menu/src/components/Register.js
@@ -1,48 +1,97 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './css/login.css';
 
 // 定义样式
 const styles = {
+  backgroundImage: {
+    backgroundImage: `url('/login.png')`,
+    backgroundSize: 'cover', // 使用 'cover' 使背景图覆盖整个容器
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'fixed', // 固定背景图
+    top: 0,
+    left: 0,
+    width: '100%', // 让宽度为 100%
+    height: '100%', // 让高度为 100%
+    zIndex: -1, // 确保背景在最底层
+  }, 
   registerContainer: {
-    maxWidth: '400px',
-    margin: '150px auto',
-    padding: '30px',
+    maxWidth: '300px',
+    maxHeight: '500px',
+    margin: '100px auto',
+    padding: '30px 50px 100px',
     borderRadius: '8px',
     boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    backgroundColor: '#f9f9f9',
+    background: 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.2))', // 渐变背景，边缘更黑
+    //opacity: 0.7, // 背景透明度
+    transition: 'all 0.3s ease',
+  },
+  WebTitle: {
+    textAlign: 'center',
+    marginBottom: '30px',
+    fontSize: '44px',
+    fontWeight: 'bold',
+    background: 'linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)', // 设置您想要的渐变背景
+    WebkitBackgroundClip: 'text', // 剪裁背景到文本
+    color: 'transparent', // 文字颜色设置为透明
   },
   registerTitle: {
     textAlign: 'center',
-    marginBottom: '20px',
-    color: '#333',
+    marginBottom: '30px',
+    color: 'white',
+    fontSize: '28px',
+    fontWeight: 'bold',
   },
   formGroup: {
-    marginBottom: '15px',
+    marginBottom: '20px',
   },
   formInput: {
-    width: '100%',
-    padding: '10px',
-    borderRadius: '4px',
-    border: '1px solid #ccc',
-    boxShadow: 'inset 0 1px 3px rgba(0, 0, 0, 0.1)',
-  },
-  registerButton: {
-    width: '100%',
+    width: '70%',
     padding: '10px',
     border: 'none',
-    borderRadius: '4px',
-    backgroundColor: '#007bff',
+    borderBottom: '3px solid white',
+    boxShadow: 'none',
+    transition: 'border-color 0.02s',
+    backgroundColor: 'transparent',
+    zIndex: 1,
+    outline: 'none',
+    color: 'white',
+    opacity: 0.8,
+    fontSize: '14px',
+  },
+  formInputFocus: {
+    borderColor: '1px solid white',
+    outline: 'none',
+  },
+  registerButton: {
+    marginBottom: '20px',
+    width: '50%',
+    textAlign: 'center',
+    padding: '10px',
+    border: 'none',
+    borderRadius: '20px',
+    background: 'linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)',
     color: 'white',
     fontSize: '16px',
     cursor: 'pointer',
+    transition: 'background-color 0.2s',
+  },
+  loginButtonHover: {
+    backgroundColor: '#0056b3',
   },
   loginLink: {
     display: 'block',
+    bottom: '10px',
+    left: '95%',
     textAlign: 'right',
     marginTop: '15px',
-    color: '#007bff',
+    color: 'white',
+    opacity: 0.6,
     cursor: 'pointer',
+    fontSize: '12px',
+    textDecoration: 'underline',
   },
 
 };
@@ -72,35 +121,40 @@ function Register() {
   };
 
   return (
-    <div style={styles.registerContainer}>
-      <h2 style={styles.registerTitle}>注册账号</h2>
-      <form onSubmit={handleRegister}>
-        <div style={styles.formGroup}>
-          <label>用户名:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            style={styles.formInput}
-          />
-        </div>
-        <div style={styles.formGroup}>
-          <label>密码:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={styles.formInput}
-          />
-        </div>
-        <button type="submit" style={styles.registerButton}>
-          注册
-        </button>
+    <div style={styles.backgroundImage}>
+      <div style={styles.registerContainer}>
+        <h2 style={styles.WebTitle}>BOUNTY INN</h2>
+        <h2 style={styles.registerTitle}>Register</h2>
+        <form onSubmit={handleRegister}>
+          <div style={styles.formGroup}>
+            <i className="fas fa-user" style={{ color: 'white', marginRight: '10px' }}></i> {/* 用户图标 */}
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              style={styles.formInput}
+              placeholder="UserName"
+            />
+          </div>
+          <div style={styles.formGroup}>
+            <i className="fas fa-lock" style={{ color: 'white', marginRight: '10px' }}></i> {/* 锁图标 */}
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={styles.formInput}
+              placeholder='UserPassword'
+            />
+          </div>
+          <button type="submit" style={styles.registerButton}>
+            Sign up
+          </button>
 
-      </form>
-      <div style={styles.loginLink} onClick={() => navigate('/')}>
-        返回登录
-        </div>
+        </form>
+        <div style={styles.loginLink} onClick={() => navigate('/')}>
+          Back
+          </div>
+      </div>
     </div>
   );
 }
